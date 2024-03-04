310c6d4ebadffc7f548190eb747f636eaee222d7
tetsu-777 <ta2ga3te28@gmail.com>
2024-03-05 00:09:30 +0900

    API information まとめ

--- commit_only_api.py
+++ commit_only_api.py
@@ -4,7 +4,20 @@ import json
 DEBUG = True
 GITLAB_API_ENDOPOINT = 'https://gitlab.example.com/api/v4'
 
+
 class gitlab:
+
+    """API通信時に必要な情報をまとめたい（Issue/Branches/Commits/Merge Requests API通信時のリクエストで共通の使用するデータを毎回APIを呼び出したくない、、）
+        api_information = {
+            "mr_title": string(MRのタイトル),
+            "issue_id": int(作成したIssueのID),
+            "source_branch": string(開発ブランチ名),
+            "assignee_id": int(ユーザーID),
+            "mr_description": string(MR概要)
+        }
+    """
+    api_information = {}
+
     def __init__(self, url, personal_access_token, source_project_name, source_commit_sha, target_project_name, target_branch_name):
         self.url = url
         self.personal_access_token = personal_access_token
@@ -17,7 +30,7 @@ class gitlab:
     def get_file_changes_from_commit_sha(self):
         """ コミットsha番号からファイルの変更情報を取得する
             Args:
-                self(gitlab class) : 第一引数
+                None
             Returns
                 list: file_changes(ファイルの変更情報)
                 期待されるデータ例
@@ -50,3 +63,34 @@ class gitlab:
         # json.loads() 関数を使用して文字列をPythonのリスト型に変換
         file_changes = json.loads(output)
         return file_changes
+
+
+    def commit_cherrypick_information_for_mr(self):
+        """ コミット単位でのcherrypick実行時の際、MR作成に必要な情報（title, description）を取得・成形する
+            Args:
+                None
+            Returns
+                None
+        """
+        # titleデータの取得・成形
+        api_url = f'{GITLAB_API_ENDOPOINT}/projects/{self.source_project_name}/repository/commits/{self.source_commit_sha}'
+        header = f'PRIVATE-TOKEN: {self.personal_access_token}'
+        try:
+            result = subprocess.run(['curl', '--header', header, '--url', api_url], capture_output=True, text=True, check=True)
+        except Exception as e:
+            if DEBUG:
+                print(f'subprocess command error: {e}')
+            raise Exception('subprocessコマンドの実行に失敗しました。\nコミットsha番号:{self.source_commit_sha}')
+        output = result.stdout
+        if DEBUG:
+            print(f'Commits API response:\n{output}')
+        if '404' in output:
+            raise Exception(f'指定のコミットを取得できませんでした。\nコミットsha番号:{self.source_commit_sha}')
+        # json.loads() 関数を使用して文字列をPythonの辞書型に変換
+        commit_infomation = json.loads(output)
+
+        self.api_information['mr_title'] = commit_infomation['title']
+
+        description = f'{commit_infomation['title']}\n{commit_infomation['short_id']}'
+        self.api_information['description'] = description
+
